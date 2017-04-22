5ee825bce665ce79702b791f9d6cd838188ba701
davidsilwal <de.davidsilwal@gmail.com>
2017-04-22 11:40:52 +0545

    end

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    remoteUrl : 'mongodb://david-gabc:2gxK7XWSSmK7CAb36lXHJM0hNZQMO838PeMR1tiiBhWJgBR509mMjjmJN7W5qgEkDIbpoRN9xtKThmvvVxM4bg%3D%3D@david-gabc.documents.azure.com:10250?ssl=true&authSource=admin',
+    remoteUrl: 'mongodb://david-gabc:2gxK7XWSSmK7CAb36lXHJM0hNZQMO838PeMR1tiiBhWJgBR509mMjjmJN7W5qgEkDIbpoRN9xtKThmvvVxM4bg==@david-gabc.documents.azure.com:10250/?ssl=true',
 };
